9653fa34077d778c6138aec3ae2ab467bfd800ed
Tung Nguyen <tung.nguyenthanh@shopee.com>
2020-02-21 11:18:59 +0700

    Add some field data to response

--- story/spiders/ttv_book.py
+++ story/spiders/ttv_book.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import re
+import datetime
 
 
 class TTVBookSpider(scrapy.Spider):
@@ -20,10 +22,35 @@ class TTVBookSpider(scrapy.Spider):
         super().__init__(**kwargs)
 
     def parse(self, response):
+        is_full = "Đã hoàn thành" in response.css('.book-information .book-info .tag span::text').getall()
         author = response.css('.author-info .author-photo p a::text').get(default='').strip()
         cats = response.css('.book-information .book-info .tag a::text').getall()
         if author in cats:
             cats.remove(author)
+
+        source_like = response.css('.book-information .book-info').xpath("//p//span[re:test(@class, '\.*-like$')]/text()").get(default='').strip()
+        if source_like:
+            source_like = int(source_like)
+
+        source_view = response.css('.book-information .book-info').xpath("//p//span[re:test(@class, '\.*-view$')]/text()").get(default='').strip()
+        if source_view:
+            source_view = int(source_view)
+
+        source_follow = response.css('.book-information .book-info').xpath("//p//span[re:test(@class, '\.*-follow$')]/text()").get(default='').strip()
+        if source_follow:
+            source_follow = int(source_follow)
+
+        source_last_update = response.css('.catalog-content-wrap').xpath('//h3//em//text()').get(default='')
+        match = re.compile(r'.+ (\d{0,2})\/(\d{0,2})\/(\d{4}) (\d{0,2}):(\d{0,2}).+').match(source_last_update)
+        if match:
+            date, month, year, hour, minute = match.groups()
+            date = datetime.datetime(int(year), int(month), int(date), int(hour), int(minute))
+            source_last_update = date.isoformat()
+
+        source_total_chapter = response.css('.content-nav-wrap .nav-wrap').xpath('//ul//li//a[@onclick=$val]//text()', val='changeTab(2)').get()
+        match = re.compile(r'.*\((\d{1,10}).*').match(source_total_chapter)
+        if match:
+            source_total_chapter = int(match.groups()[0])
 
         story_data = {
             'book': json.dumps({
@@ -32,6 +59,12 @@ class TTVBookSpider(scrapy.Spider):
                 'img': response.css('.book-information .book-img img').attrib['src'].strip(),
                 'author': author,
                 'cats': json.dumps(cats),
+                'is_full': 1 if is_full else 0,
+                'source_like': source_like,
+                'source_view': source_view,
+                'source_follow': source_follow,
+                'source_last_update': source_last_update,
+                'source_total_chapter': source_total_chapter,
                 'id': self.id,
             }),
             'redis_stream_name': self.redis_stream_name
